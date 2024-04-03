89654232b1d03091bad44790d39598e8ce82cff8
Mario Krstevski <mariokrstevski@hotmail.com>
2024-04-03 12:41:43 -0400

    revert na promena na ima

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React in SMX
+          Learn React
         </a>
       </header>
     </div>

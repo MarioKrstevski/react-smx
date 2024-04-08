c97e0f7e6d8eaad122db02037ade0b99dd105b01
Mario Krstevski <mariokrstevski@hotmail.com>
2024-04-08 13:25:32 -0400

    different props

--- src/classes/C2.js
+++ src/classes/C2.js
@@ -27,9 +27,59 @@ function Button({ variant, className }) {
     </button>
   );
 }
+
+function DiscoveringProps(props) {
+  return <>{props.number}</>;
+}
+
+function DiscoveringProps(props) {
+  const { number = 10, string, func } = props;
+  return <>{number}</>;
+}
+
+function DiscoveringProps(props) {
+  const number = props.number || 10;
+  const string = props.string;
+  const func = props.func;
+  return <>{number}</>;
+}
+function DiscoveringProps({
+  number = 10,
+  string,
+  func,
+  obj,
+  array,
+  nan,
+  bool,
+  defaultt,
+  name = "John Smith",
+}) {
+  return (
+    <>
+      {number}
+
+      {}
+    </>
+  );
+}
+// JSX
 export default function C2() {
   return (
     <div>
+      <DiscoveringProps
+        number={9}
+        string="text"
+        obj={{ a: 5 }}
+        array={[5, 4, 3]}
+        bool={false}
+        defaultt
+        nan={NaN}
+        func={function test(params) {}}
+        func2={test()}
+        // name= "Mario"
+      />
+      {console.log()}
+      {[]} {{}}
       {/* {add(5, 6)}
       <br />
       {add(5, 5)}
@@ -37,10 +87,8 @@ export default function C2() {
       {add(5, 3)}
       <br />
       <Add x={5} y={10} /> */}
-
-      <Button variant={"primary"} className="smx" />
-      <Button variant="secondary" />
-
+      {/* <Button variant={"primary"} className="smx" />
+      <Button variant="secondary" /> */}
       {/* <Person name="Mario" />
       <Person name="Simona" />
       <Person name="Marina" />

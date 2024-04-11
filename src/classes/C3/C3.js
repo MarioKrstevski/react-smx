2181532a06914f39db939e60261086bcfa86c809
Mario Krstevski <mariokrstevski@hotmail.com>
2024-04-11 00:34:49 -0400

    H3 homework

--- src/classes/C3/C3.js
+++ src/classes/C3/C3.js
@@ -23,13 +23,120 @@ const preparedButton = (
     Children Two <img src="" alt="test" />
   </Button>
 );
-console.log(preparedButton);
+// console.log(preparedButton);
+
+const people = [
+  {
+    id: 0,
+    name: "Creola Katherine Johnson",
+    profession: "mathematician",
+  },
+  {
+    id: 1,
+    name: "Mario José Molina-Pasquel Henríquez",
+    profession: "chemist",
+  },
+  {
+    id: 2,
+    name: "Mohammad Abdus Salam",
+    profession: "physicist",
+  },
+  {
+    id: 3,
+    name: "Percy Lavon Julian",
+    profession: "chemist",
+  },
+  {
+    id: 4,
+    name: "Subrahmanyan Chandrasekhar",
+    profession: "astrophysicist",
+  },
+];
+
+//If the person is a chemist, then the name should be
+// with a red color, otherwise black (as normal)
+
+function People({ listItems }) {
+  console.log(listItems);
+
+  // if() {} else {} ----- ? :  ----- &&
+
+  return (
+    <div>
+      {listItems.map((person) => {
+        return (
+          <div
+            style={{
+              color:
+                person.profession === "chemist" ? "red" : "black",
+            }}
+            key={person.id}
+          >
+            {person.name} {person.profession === "chemist" && "💉💉"}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+function ShoppingList({ list }) {
+  return (
+    <ul
+      style={{
+        textAlign: "left",
+      }}
+    >
+      {list.map((product) => {
+        if (product.done) {
+          return (
+            <li key={product.id}>
+              <span
+                style={{
+                  textDecoration: "line-through",
+                }}
+              >
+                {product.name}
+              </span>
+              <span style={{ marginLeft: 10 }}>{"  ✅"}</span>
+            </li>
+          );
+        } else {
+          return <li key={product.id}>{product.name}</li>;
+        }
+      })}
+    </ul>
+  );
+}
+
+const RandomButton = () => {
+  // Generate random values for styles
+  const randomFontSize = Math.floor(Math.random() * 20 + 12) + "px"; // Random font size between 12px and 32px
+  const randomBorderRadius = Math.floor(Math.random() * 20) + "px"; // Random border radius between 0px and 20px
+  const randomColor =
+    "#" + Math.floor(Math.random() * 16777215).toString(16); // Random color
+  const randomBackgroundColor =
+    "#" + Math.floor(Math.random() * 16777215).toString(16); // Random background color
+
+  // Define button style object with random styles
+  const buttonStyle = {
+    fontSize: randomFontSize,
+    borderRadius: randomBorderRadius,
+    color: randomColor,
+    backgroundColor: randomBackgroundColor,
+    padding: "10px 20px",
+    border: "none",
+    cursor: "pointer",
+  };
+
+  return <button style={buttonStyle}>Random Button</button>;
+};
 
 export default function C3(props) {
   return (
     <div>
       {/* <Lists /> */}
-      <Styling />
+      {/* <Styling /> */}
       {/* 
       <EventHandlers
         onClick={function () {
@@ -37,8 +144,25 @@ export default function C3(props) {
           alert("Passed from above");
         }}
       />
-
       <State /> */}
+
+      {/* Below this are exercises during class */}
+      <hr />
+
+      <People listItems={people} />
+      <hr />
+      <ShoppingList
+        list={[
+          { id: 1, name: "Milk", done: false },
+          { id: 2, name: "Eggs", done: false },
+          { id: 3, name: "Bread", done: true },
+          { id: 4, name: "Apples", done: false },
+          { id: 5, name: "Chicken", done: false },
+          { id: 6, name: "Rice", done: true },
+        ]}
+      />
+
+      <RandomButton />
     </div>
   );
 }

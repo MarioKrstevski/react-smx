d5c9b5233aec8f149e79b756d5d036600e8c58db
Mario Krstevski <mariokrstevski@hotmail.com>
2024-04-19 13:12:51 -0400

    useRef

--- src/classes/C7/ColorPicker.js
+++ src/classes/C7/ColorPicker.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
-export default function ColorPicker(props) {
+function ColorPicker(props) {
   const [color, setColor] = useState("white");
 
   function handleColorChange(event) {
@@ -44,3 +44,65 @@ export default function ColorPicker(props) {
     </div>
   );
 }
+
+function ColorPicker2(props) {
+  const [color, setColor] = useState("white");
+  const clickCount = useRef();
+  let clickCount2 = 0;
+
+  const colorInputRef = useRef();
+  function click() {
+    clickCount2++;
+    clickCount.current++;
+  }
+
+  function handleColorChange(event) {
+    setColor(event.target.value);
+  }
+
+  function handleClick() {
+    // const colorInput = document.getElementById("color");
+    // colorInput.click();
+
+    colorInputRef.current.click();
+  }
+
+  return (
+    <div>
+      <div
+        onClick={handleClick}
+        style={{
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <div
+          className="box"
+          style={{
+            height: "20px",
+            display: "inline-block",
+            width: "20px",
+            borderRadius: "50%",
+            border: "1px solid black",
+            margin: 5,
+            backgroundColor: color,
+          }}
+        ></div>{" "}
+        <span>{color}</span>
+      </div>
+
+      <input
+        ref={colorInputRef}
+        style={{
+          visibility: "hidden",
+        }}
+        type="color"
+        id="color"
+        value={color}
+        onChange={handleColorChange}
+      />
+    </div>
+  );
+}
+
+export default ColorPicker2;
